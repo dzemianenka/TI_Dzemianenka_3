35d5529e23ebaea6d27b13b571d2f279d750a87a
Aliaksei Dzemianenka <aliaksei.dzemianenka@sap.com>
2022-04-07 16:01:58 +0400

    Initial commit

---
+++ src/test/java/com/dzemianenka/TiDzemianenka3ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.dzemianenka;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiDzemianenka3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
